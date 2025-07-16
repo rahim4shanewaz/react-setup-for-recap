af132f97089a7df7a412b08055144479de4f2280
rahim4shanewaz <soumiq444@gmail.com>
2025-07-16 23:37:24 +0600

    react install, setup, revise useState

---
+++ src/Counter.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+export default function(){
+
+
+    const[count, setCount] = useState(0)
+
+
+
+
+
+
+    return(
+
+        <h3>
+            Counter:
+        </h3>
+    )
+}
